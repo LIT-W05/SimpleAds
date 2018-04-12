666e7c74dba949b9d1f6db261e579e42b51d3947
Avrumi Friedman <alex+5@lakewoodprogramming.com>
2018-04-11 22:34:58 -0400

    Initial commit

---
+++ SimpleAds.Web/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleAds.Data;
+using System.IO;
+using SimpleAds.Web.Models;
+
+namespace SimpleAds.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            SimpleAdDb db = new SimpleAdDb(Properties.Settings.Default.ConStr);
+            IEnumerable<SimpleAd> ads = db.GetAds();
+            List<string> ids = new List<string>();
+            if (Request.Cookies["AdIds"] != null)
+            {
+                ids = Request.Cookies["AdIds"].Value.Split(',').ToList();
+            }
+
+            return View(new HomePageViewModel
+            {
+                Ads = ads.Select(ad =>
+                {
+                    return new AdViewModel
+                    {
+                        Ad = ad,
+                        CanDelete = ids.Contains(ad.Id.ToString())
+                    };
+                })
+            });
+        }
+
+        public ActionResult NewAd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewAd(SimpleAd ad)
+        {
+            SimpleAdDb db = new SimpleAdDb(Properties.Settings.Default.ConStr);
+            db.AddSimpleAd(ad);
+            string ids = "";
+            HttpCookie cookie = Request.Cookies["AdIds"];
+            if (cookie != null)
+            {
+                ids = $"{cookie.Value},";
+            }
+            ids += ad.Id;
+            Response.Cookies.Add(new HttpCookie("AdIds", ids));
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAd(int id)
+        {
+            SimpleAdDb db = new SimpleAdDb(Properties.Settings.Default.ConStr);
+            db.Delete(id);
+            return Redirect("/");
+        }
+    }
+}
---
+++ SimpleAds.Web/Models/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleAds.Data;
+
+namespace SimpleAds.Web.Models
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<AdViewModel> Ads { get; set; }
+    }
+}
